dcae7418d952bc67904535281c44da0f5b59f42c
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-04-20 10:29:58 +0530

    Create 0380. Insert Delete GetRandom O(1).cpp

---
+++ 0380. Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,40 @@
+class RandomizedSet {
+public:
+    RandomizedSet() {
+        
+    }
+    map<int,int>mp;
+    vector<int>ans;
+  
+    bool insert(int val) {
+        if(mp.find(val)==mp.end()){
+        ans.push_back(val);
+        mp[val]=ans.size()-1;
+        }
+        else return false;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(mp.find(val)!=mp.end()){
+            mp[ans.back()]=mp[val];
+         swap(ans.back(),ans[mp[val]]);
+         ans.pop_back();
+        mp.erase(val);
+        }
+        else return false;
+        return true;
+    }
+    
+    int getRandom() {
+        return ans[rand()%mp.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
