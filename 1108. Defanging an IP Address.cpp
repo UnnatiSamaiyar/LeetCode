99dfeeb6b0e4b2e2dd9b54bce68d375dc6b40599
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-05-03 15:46:14 +0530

    Create 1108. Defanging an IP Address.cpp

---
+++ 1108. Defanging an IP Address.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s;
+        for(char c: address){
+            if(c=='.'){
+                s += "[.]";
+            }
+            else{
+                s += c;
+            }
+        }
+        return s;
+    }
+};
