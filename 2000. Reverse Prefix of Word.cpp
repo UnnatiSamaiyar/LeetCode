1e6f1cdc841a00905a08e895c3ea46351af7aa61
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-09-12 14:29:33 +0530

    Create 2000. Reverse Prefix of Word.cpp

---
+++ 2000. Reverse Prefix of Word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for(int i=0; i<word.length(); i++){
+            if(ch==word[i]){
+                reverse(word.begin(), word.begin()+i+1);
+                break;
+            }
+        }
+        return word;
+    }
+};
